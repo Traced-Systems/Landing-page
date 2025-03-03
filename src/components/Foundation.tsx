e5a03fb4b56c04e8ba1f0c6edc9679c5b156f689
zoi23333 <ziwei.chen@chromaway.com>
2025-03-03 15:05:30 +0100

    High quality Logo Image

--- src/components/Foundation.tsx
+++ src/components/Foundation.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Card } from "@/components/ui/card";
 const Foundation = () => {
-  return <section className="w-full bg-[#f7f7f7] py-16 pt-20">
+  return (
+    <section className="w-full bg-[#f7f7f7] py-16 pt-20">
       <div className="container mx-auto px-4 flex flex-col items-center">
         <h2 className="text-3xl font-medium text-[#1E293B] mb-8 text-center">
           The Foundation of Our Innovation
@@ -15,14 +16,38 @@ const Foundation = () => {
                 Funded by
               </span>
               <div className="flex flex-wrap  items-center justify-center gap-12 lg:gap-16 ">
-                <a href="https://chromaway.com/" target="_blank" rel="noopener noreferrer">
-                  <img src="/lovable-uploads/1000002907.svg" alt="ChromaWay" className="h-12 xl:h-16" />
+                <a
+                  href="https://chromaway.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="/lovable-uploads/1000002907.svg"
+                    alt="ChromaWay"
+                    className="h-12 xl:h-16"
+                  />
                 </a>
-                <a href="https://commission.europa.eu/index_en" target="_blank" rel="noopener noreferrer">
-                  <img src="/lovable-uploads/ff086cb2-38b6-4ca2-a158-69f69cf213ee.png" alt="European Commission" className="h-12 xl:h-16" />
+                <a
+                  href="https://commission.europa.eu/index_en"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="/lovable-uploads/ff086cb2-38b6-4ca2-a158-69f69cf213ee.png"
+                    alt="European Commission"
+                    className="h-12 xl:h-16"
+                  />
                 </a>
-                <a href="https://ec.europa.eu/digital-building-blocks/sites/display/EBSI/Home" target="_blank" rel="noopener noreferrer">
-                  <img src="/lovable-uploads/e1307a99-b10d-4799-b274-47c5f4487c18.png" alt="EBSI" className="h-16 xl:h-20 mt-2" />
+                <a
+                  href="https://ec.europa.eu/digital-building-blocks/sites/display/EBSI/Home"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="/lovable-uploads/e1307a99-b10d-4799-b274-47c5f4487c18.png"
+                    alt="EBSI"
+                    className="h-16 xl:h-20 mt-2"
+                  />
                 </a>
               </div>
             </div>
@@ -35,13 +60,22 @@ const Foundation = () => {
               <span className="text-[#1E293B] text-lg font-base mb-4">
                 Powered by
               </span>
-              <a href="https://chromia.com/" target="_blank" rel="noopener noreferrer">
-                <img alt="Chromia" className="h-12 xl:h-16 mt-2 mb-1" src="/lovable-uploads/f916c022-de91-44d1-9ab2-5acc9aa341e3.png" />
+              <a
+                href="https://chromia.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  alt="Chromia"
+                  className="h-16 xl:h-16 mt-2 mb-1"
+                  src="/lovable-uploads/8efc7792-2c07-415e-9ab8-cac82841e51a.png"
+                />
               </a>
             </div>
           </div>
         </Card>
       </div>
-    </section>;
+    </section>
+  );
 };
 export default Foundation;
